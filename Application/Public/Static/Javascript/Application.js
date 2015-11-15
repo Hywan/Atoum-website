f74f4aecb0d772e2b3dd8418901f6b8e2f2a6672
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-11-15 22:46:44 +0100

    Source: End of the draft.

--- Application/Public/Static/Javascript/Application.js
+++ Application/Public/Static/Javascript/Application.js
@@ -43,6 +43,40 @@ Application.ApplicationRoute = Ember.Route.extend({
 
 });
 
+Application.SourcesRoute = Ember.Route.extend({
+
+    model: function ()
+    {
+        return new Ember.RSVP.Promise(
+            function (resolve, reject) {
+                $.getJSON(
+                    'https://api.github.com/repos/atoum/atoum/releases/latest'
+                ).done(function (data) {
+                    resolve({
+                        releases: {
+                            latest: {
+                                name: data.name,
+                                page_url: data.html_url,
+                                tarball_url: data.tarball_url,
+                                zipball_url: data.zipball_url,
+                                pharball_url: data.assets.find(function (asset) {
+                                    return 'atoum.phar' === asset.name;
+                                }).browser_download_url
+                            }
+                        },
+                        composer: {
+                            latest_version: '~' + data.tag_name.split('.')[0] + '.0',
+                            current_x: data.tag_name.split('.')[0],
+                            next_x: parseInt(data.tag_name.split('.')[0]) + 1
+                        }
+                    });
+                });
+            }
+        );
+    }
+
+});
+
 Application.SuperCodeComponent = Ember.Component.extend({
 
     tagName   : 'code',
@@ -66,3 +100,35 @@ Application.SuperCodeComponent = Ember.Component.extend({
     }
 
 });
+
+
+/**
+ * Polyfills.
+ */
+
+if (!Array.prototype.find) {
+    Array.prototype.find = function(predicate) {
+        if (this === null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+
+        var list    = Object(this);
+        var length  = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+
+        return undefined;
+    };
+}
